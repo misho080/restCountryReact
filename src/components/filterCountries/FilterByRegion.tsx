0f0eca5f357363cd2a12ae560c6d10f2b71d5f0c
misho goshadze <mishikogoshadze28@gmail.com>
2025-04-11 21:41:51 +0400

    fix linkStyle problem

--- src/components/filterCountries/FilterByRegion.tsx
+++ src/components/filterCountries/FilterByRegion.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 const FilterByRegion = () => {
     return (
         <div className="filterContainer">
-            <Link to="/africa"><p className="regionName">Africa</p></Link>
-            <Link to={"/america"}><p className="regionName">America</p></Link>
-            <Link to={"/asia"}><p className="regionName">Asia</p></Link>
-            <Link to={"/europe"}><p className="regionName">Europe</p></Link>
-            <Link to={"/oceania"}><p className="regionName">Oceania</p></Link>
+            <Link className="filterLink" to="/africa"><p className="regionName">Africa</p></Link>
+            <Link className="filterLink" to={"/america"}><p className="regionName">America</p></Link>
+            <Link className="filterLink" to={"/asia"}><p className="regionName">Asia</p></Link>
+            <Link className="filterLink" to={"/europe"}><p className="regionName">Europe</p></Link>
+            <Link className="filterLink" to={"/oceania"}><p className="regionName">Oceania</p></Link>
         </div>
     )
 }
